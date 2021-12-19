e1c914acb6983460d176fdba1cd6b70a42514272
User <taranovegor91@gmail.com>
2021-12-19 23:49:18 +0300

    Динамическое добавление трех точек в случае длинного названия команды и еще одно поле сверстал

--- app/src/main/java/com/example/werstkalesson19/MainActivity.kt
+++ app/src/main/java/com/example/werstkalesson19/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.werstkalesson19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvNapoli :TextView
+    lateinit var tvLazio :TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvNapoli=findViewById(R.id.tvNapoli)
+        tvLazio=findViewById(R.id.tvLazio)
+        var txtNapoli:String=tvNapoli.text.toString()
+        if(txtNapoli.length>10){
+            tvNapoli.setText(txtNapoli.substring(0,8)+"...")
+        }
+        var txtLazio:String=tvLazio.text.toString()
+        if(txtLazio.length>10){
+            tvLazio.text=txtLazio.substring(0,8)+"..."
+        }
     }
 }
